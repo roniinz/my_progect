814f9a7a3b91a3c8b1a56f690e44dc5f13376d30
roniinx <roniinx@gmail.com>
2019-08-20 09:13:33 +0300

    Все сделанно по заданию и модель и так далее удалил модель многи со смногими ... ибо безсмысленно

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/img')
+MEDIA_URL = '/media/img/'
